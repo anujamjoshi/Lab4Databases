eaf0b5e73347124d66119670e50521d0a6701724
Anuja Joshi <anuja.j20@gmail.com>
2017-11-20 19:29:40 -0800

    working on tables

--- src/Driver.java
+++ src/Driver.java
@@ -6,28 +6,70 @@ public class Driver {
 		try {
 			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/lab4", "root" , "Password");
 			Statement myStat= myConn.createStatement();
-			 System.out.println("Connected database successfully...");
-			 System.out.println("Creating table in given database...");
-		    
-		      
-		      String sql = "CREATE TABLE REGISTRATION " +
-		                   "(id INTEGER not NULL, " +
-		                   " first VARCHAR(255), " + 
-		                   " last VARCHAR(255), " + 
-		                   " age INTEGER, " + 
-		                   " PRIMARY KEY ( id ))"; 
+			System.out.println("Connected database successfully...");
+			System.out.println("Creating tables...");
+			//createTables(myStat);
 
-		      myStat.executeUpdate(sql);
-		      System.out.println("Created table in given database...");
-			
+			//		      
+			String sql = "DROP TABLE REGISTRATION ";
+
+			myStat.executeUpdate(sql);
+
 		}
-		
-		
-		
+
+
+
 		catch (Exception e ) {
 			e.printStackTrace();
 		}
 
+	}
+
+
+
+	public static void createTables(Statement myStat) {
+		String TripOffering = "CREATE TABLE TripOffering " +
+				"("+ 
+				"TripNumber INTEGER not NULL, " +
+				" Date VARCHAR(10), " + 
+				" StartTime VARCHAR(5), " + 
+				" ArrivalTime VARCHAR(5), " + 
+				"DriverName VARCHAR(25), " + 
+				"BusID VARCHAR(25), " + 
+				" PRIMARY KEY ( TripNumber, Date, StartTime )"
+				+ ")"; 
+
+		String Bus = "CREATE TABLE Bus " +
+				"("+ 
+				"BusID VARCHAR(25), " +
+				" Model VARCHAR(25), " + 
+				" Year INTEGER," + 
+				" PRIMARY KEY ( BusID ),"+ 
+				"FOREIGN KEY (BusID) REFERENCES TripOffering(BusID)"+
+				")"; 
+
+		String Driver = "CREATE TABLE Driver " +
+				"("+ 
+				"DriverName VARCHAR(25), " +
+				" DriverTelephoneNumber INTEGER, " + 
+				" PRIMARY KEY ( DriverName ),"+ 
+				"FOREIGN KEY (DriverName) REFERENCES TripOffering(DriverName)"+ 
+				")"; 
+		String Trip = "CREATE TABLE Trip " +
+				"("+ 
+				"TripNumber INTEGER not NULL," +
+				" StartLocationName VARCHAR(30), " + 
+				"DestinationName VARCHAR (30)"+ 
+				" PRIMARY KEY ( DriverName ),"+ 
+				"FOREIGN KEY (TripNumber) REFERENCES TripOffering(TripNumber)"+ 
+				")"; 
+
+		try {
+			myStat.executeUpdate(TripOffering);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
