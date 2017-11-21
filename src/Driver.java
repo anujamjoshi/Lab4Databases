081c4addd0271b372346a141f6b19bd97706056f
Anuja Joshi <anuja.j20@gmail.com>
2017-11-20 19:58:30 -0800

    Tables Made

--- src/Driver.java
+++ src/Driver.java
@@ -32,11 +32,11 @@ public class Driver {
 				"("+ 
 				"TripNumber INTEGER not NULL, " +
 				" Date VARCHAR(10), " + 
-				" StartTime VARCHAR(5), " + 
-				" ArrivalTime VARCHAR(5), " + 
+				" ScheduledStartTime VARCHAR(5), " + 
+				" SecheduledArrivalTime VARCHAR(5), " + 
 				"DriverName VARCHAR(25), " + 
 				"BusID VARCHAR(25), " + 
-				" PRIMARY KEY ( TripNumber, Date, StartTime )"
+				" PRIMARY KEY ( TripNumber, Date, ScheduledStartTime )"
 				+ ")"; 
 
 		String Bus = "CREATE TABLE Bus " +
@@ -60,10 +60,42 @@ public class Driver {
 				"TripNumber INTEGER not NULL," +
 				" StartLocationName VARCHAR(30), " + 
 				"DestinationName VARCHAR (30)"+ 
-				" PRIMARY KEY ( DriverName ),"+ 
+				" PRIMARY KEY ( TripNumber ),"+ 
 				"FOREIGN KEY (TripNumber) REFERENCES TripOffering(TripNumber)"+ 
 				")"; 
-
+		String TripStopInfo  = "CREATE TABLE TripStopInfo  " +
+				"("+ 
+				"TripNumber INTEGER not NULL," +
+				" StopNumber INTEGER, " + 
+				"SequenceNumber INTEGER,"+
+				"DrivingTime INTEGER "+
+				" PRIMARY KEY ( TripNumber, StopNumber ),"+ 
+				"FOREIGN KEY (TripNumber) REFERENCES Trip(TripNumber)"+ 
+				")"; 
+		String ActualTripStopInfo   = "CREATE TABLE ActualTripStopInfo   " +
+				"("+ 
+				"TripNumber INTEGER not NULL, " +
+				" Date VARCHAR(10), " + 
+				" ScheduledStartTime VARCHAR(5), " + 
+				" StopNumber INTEGER, " + 
+				" SecheduledArrivalTime VARCHAR(5), " + 
+				" ActualStartTime VARCHAR(5), " + 
+				" ActualArrivalTime VARCHAR(5), " + 
+				" NumberOfPassengerIn INTEGER, " + 
+				" NumberOfPassengerOut INTEGER, " + 
+				" PRIMARY KEY ( TripNumber, Date, ScheduledStartTime, StopNumber ),"+
+				"FOREIGN KEY (TripNumber) REFERENCES Trip(TripNumber)"+ 
+				")"; 
+		String Stop = "CREATE TABLE Stop" +
+				"("	+ 
+				" StopNumber INTEGER, " + 
+				" StopAddress VARCHAR(30), " + 
+				" PRIMARY KEY ( StopNumber ),"+
+				"FOREIGN KEY (StopNumber) REFERENCES TripStopInfo(StopNumber)"+ 
+				"FOREIGN KEY (StopNumber) REFERENCES ActualTripStopInfo(StopNumber)"+ 
+				
+				") "
+				;
 		try {
 			myStat.executeUpdate(TripOffering);
 		} catch (SQLException e) {
